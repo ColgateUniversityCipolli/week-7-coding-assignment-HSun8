e33d80071ef4d1a744c478ba5bb0f09135384d8a
HSun8 <24sunh@gmail.com>
2025-03-11 22:03:00 -0400

    write up done

--- HW7.R
+++ HW7.R
@@ -8,60 +8,35 @@
 # prob was removed as parameter of the function, as prob would only be used in 
 # with qpois (inverse CDF)
 pois.prob <- function(x, lambda, type="<="){
-  # 
-  if (lambda < 0){
-    # lambda must be <= 0 
-    lambda = 0
-  }
-  if (x < 0){
-    # x must be <= 0
-    x = 0
-  }
   if (type == "="){
     # definition of PMF
-    dpois(x, lambda)
+    return(dpois(x, lambda))
   }
   if (type == "!="){
     # complement rule
-    1-dpois(x, lamda)
+    return(1-dpois(x, lambda))
   }
   if(type == "<="){
     # definition of CDF
-    ppois(x, lambda)
+    return(ppois(x, lambda))
   }
   if (type == "<"){
     # P(X < x) = P(X <= x-1)
-    ppois(x-1, lambda)
+    return(ppois(x-1, lambda))
   }
   if (type == ">="){
     # P(X >= x) = 1 - P(X <= x-1)
-    1-ppois(x-1, lambda)
+    return(1-ppois(x-1, lambda))
   }
   if (type == ">"){
     # P(X > x) = 1 - P(X <= x)
-    1-ppois(x, lambda)
+    return(1-ppois(x, lambda))
   }
 }
 
 # prob removed as a parameter
 # according to pbeta, alpha, beta must be nonnegative
 beta.prob <- function(x, alpha, beta, type="<="){
-  if (alpha < 0){
-    # alpha must be <= 0 
-     alpha = 0
-  }
-  if (beta < 0){
-    # beta must be <= 0 
-    beta = 0
-  }
-  if (x < 0){
-    # x must be <= 0
-    x = 0
-  }
-  if (x < 0){
-    # x must be <= 0
-    x = 0
-  }
   if (type == "="){
     # P(X = x) = 0 for all x
     return(0) 
@@ -73,11 +48,10 @@ beta.prob <- function(x, alpha, beta, type="<="){
   if(type == "<" | type == "<="){
     # P(X < x) = P(X <= x) for continuous distributions
     # definition of pbeta
-    pbeta(x, alpha, beta)
+    return(pbeta(x, alpha, beta))
   }
   if (type == ">" | type == ">="){
     # P(X > x) = P(X >= x) = 1 - P(X < x)for continouous distributions 
-    1-pbeta(x, alpha, beta)
+    return(1 - pbeta(x, alpha, beta))
   }
-  # Use dbeta and pbeta to conditionally return the correct probability
 }
